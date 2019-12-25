d2342975f23ad484a6c9c7b67461dfe17fa04494
Higo Alexandre <higoalexandreb@gmail.com>
2019-12-24 23:38:40 -0300

    Update

--- src/main/java/com/higodev/admin/AdminApplication.java
+++ src/main/java/com/higodev/admin/AdminApplication.java
@@ -1,19 +1,15 @@
 package com.higodev.admin;
 
-import com.higodev.admin.security.WebSecurityConfig;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration(excludeName = {"WebSecurityConfig"})
+@EnableAutoConfiguration
 @EnableAdminServer
 public class AdminApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(AdminApplication.class)
-                .web(WebApplicationType.REACTIVE)
-                .run(args);
+        SpringApplication.run(AdminApplication.class, args);
     }
 }
